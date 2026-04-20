265d67e2375640dd4c8d8df2a35ecc4f12230c8e
Raj Sasidharan <raj@myriad.com>
2026-04-19 19:51:50 -0700

    IRSA for ECR, Bedrock, Rag-Service app

--- argocd-apps.tf
+++ argocd-apps.tf
@@ -34,6 +34,47 @@ resource "kubectl_manifest" "hello_app" {
   })
 
   depends_on = [
+      helm_release.argocd,
+      kubernetes_secret.argocd_app_repo,
+    ]
+  }
+
+resource "kubectl_manifest" "rag_service_app" {
+  yaml_body = yamlencode({
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "rag-service"
+      namespace = kubernetes_namespace.argocd.metadata[0].name
+      finalizers = [
+        "resources-finalizer.argocd.argoproj.io",
+      ]
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = var.argocd_app_repo_url
+        targetRevision = "HEAD"
+        path           = "rag-service"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = "rag"
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+        syncOptions = [
+          "CreateNamespace=true",
+          "PrunePropagationPolicy=foreground",
+        ]
+      }
+    }
+  })
+
+  depends_on = [
     helm_release.argocd,
     kubernetes_secret.argocd_app_repo,
   ]
